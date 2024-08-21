f86e2f648fc59e459362caea89c3b1df9c44e078
Sees <Vsevolod.Egorov@ronavi.ru>
2024-08-21 18:56:08 +0300

    add classic navigation action in general launch

--- launch/general.launch.py
+++ launch/general.launch.py
@@ -3,8 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, GroupAction, IncludeLaunchDescription, ExecuteProcess
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -95,6 +94,25 @@ def generate_launch_description():
                       condition = IfCondition(LaunchConfiguration('localization_mode'))
   )
 
+  navigation_load = GroupAction([
+    ExecuteProcess(
+                cmd=[
+                    "ros2",
+                    "launch",
+                    "eureka_navigation",
+                    "localization.launch.py"
+                ]
+    ),
+    ExecuteProcess(
+                cmd=[
+                    "ros2",
+                    "launch",
+                    "eureka_navigation",
+                    "nav2.launch.py"
+                ]
+    )],
+    condition = IfCondition(LaunchConfiguration('navigation_mode'))
+  )
 
   ack_drive_spawner = Node(
     package='controller_manager',
@@ -117,6 +135,7 @@ def generate_launch_description():
   ld.add_action(mapping_node)
   ld.add_action(rviz)
   ld.add_action(localization_node)
+  ld.add_action(navigation_load)
   ld.add_action(ack_drive_spawner)
   ld.add_action(joint_broad_spawner)
   return ld
