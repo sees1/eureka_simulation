bbe9ef6ef3d473f2168568d2463b9701f70e7266
Sees <Vsevolod.Egorov@ronavi.ru>
2025-10-05 22:00:26 +0300

    feat: diff

--- launch/general.launch.py
+++ launch/general.launch.py
@@ -3,7 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, GroupAction, IncludeLaunchDescription, ExecuteProcess
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
